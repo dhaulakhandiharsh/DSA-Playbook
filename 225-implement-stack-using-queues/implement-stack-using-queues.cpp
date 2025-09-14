f09fa526e9f2d1b2ab6e6ce9e6a46a2244e276fe
harsh dhaulakhandi <134221613+furydh@users.noreply.github.com>
2025-09-14 20:31:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (55.18%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -8,7 +8,7 @@ public:
     void push(int x) {
         q.push(x);
         int size = q.size();
-        // Rotate elements to move the newly added element to the front
+       
         for(int i = 0; i < size - 1; i++) {
             q.push(q.front());
             q.pop();
