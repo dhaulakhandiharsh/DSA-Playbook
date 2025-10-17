c36e12c40b35cac089fd45f0dc6a5e2df6998926
harsh dhaulakhandi <134221613+dhaulakhandiharsh@users.noreply.github.com>
2025-10-17 16:06:11 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (88.45%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        if (n <= 1) return true;
-        priority_queue<pair<int,int>> pq;
-        vector<char> vis(n, false);
-        pq.push({0 + nums[0], 0});
-        vis[0] = true;
-        while (!pq.empty()) {
-            auto [reach, idx] = pq.top();
-            pq.pop();
-            if (reach >= n - 1) return true;
-            int limit = min(n - 1, reach);
-            for (int next = idx + 1; next <= limit; ++next) {
-                if (!vis[next]) {
-                    vis[next] = true;
-                    pq.push({next + nums[next], next});
-                }
-            }
+        int maxIndex = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(i > maxIndex) return false;
+            maxIndex = max(maxIndex, i + nums[i]);
+
         }
-        return false;
+        return true;
     }
 };
