dee3fd1b84e72844e1271c150effe7d5092961bf
harsh dhaulakhandi <134221613+dhaulakhandiharsh@users.noreply.github.com>
2025-11-02 23:48:03 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (32.78%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -4,14 +4,14 @@ public:
         int n = ratings.size();
         vector<int> candies(n, 1);
         
-        // Left to right
+       
         for (int i = 1; i < n; ++i) {
             if (ratings[i] > ratings[i - 1]) {
                 candies[i] = candies[i - 1] + 1;
             }
         }
         
-        // Right to left
+    
         for (int i = n - 2; i >= 0; --i) {
             if (ratings[i] > ratings[i + 1]) {
                 candies[i] = max(candies[i], candies[i + 1] + 1);
